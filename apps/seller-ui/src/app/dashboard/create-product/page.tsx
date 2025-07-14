c1ab374e382e3382d4756440f6b5e27f34e2c6af
theBappy <abappy575@gmail.com>
2025-07-14 10:42:49 +0600

    create seller ui updated

--- apps/seller-ui/src/app/dashboard/create-product/page.tsx
+++ apps/seller-ui/src/app/dashboard/create-product/page.tsx
@@ -1,11 +1,228 @@
+'use client'
 
+
+import ImagePlaceholder from "apps/seller-ui/src/shared/components/image-placeholder";
+import { ChevronRight } from "lucide-react";
+import ColorSelector from "packages/components/color-selector";
+import CustomProperties from "packages/components/custom-properties";
+import CustomSpecifications from "packages/components/custom-specification";
+import Input from "packages/components/input";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const CreateProduct = () => {
-  return (
-    <form>
+  const {
+    register,
+    control,
+    watch,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [openImageModal, setOpenImageModal] = useState(false);
+  const [isChanged, setIschanged] = useState(false);
+  const [images, setImages] = useState<(File | null)[]>([null]);
+  const [loading, setLoading] = useState(false);
 
+  const handleImageChange = (file: File | null, index: number) => {
+    const updatedImages = [...images];
+    updatedImages[index] = file;
+    if (index === images.length - 1 && images.length < 8) {
+      updatedImages.push(null);
+    }
+    setImages(updatedImages);
+    setValue("images", updatedImages);
+  };
+
+  const handleRemoveImage = (index: number) => {
+    setImages((prev) => {
+      let updatedImages = [...prev];
+      if (index === -1) {
+        updatedImages[0] = null;
+      } else {
+        updatedImages.splice(index, 1);
+      }
+      if (!updatedImages.includes(null) && updatedImages.length < 8) {
+        updatedImages.push(null);
+      }
+      return updatedImages;
+    });
+    setValue("images", images);
+  };
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <form
+      className="w-full mx-auto p-8 shadow-md rounded-lg text-white"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      {/* heading and breadcrumbs */}
+      <h2 className="text-2xl py-2 font-Poppins font-semibold text-white">
+        Create Product
+      </h2>
+      <div className="flex items-center">
+        <span className="text-[#80deea] cursor-pointer">Dashboard</span>
+        <ChevronRight size={20} className="opacity-[0.8]" />
+        <span>Create Product</span>
+      </div>
+      {/* content layout */}
+      <div className="py-4 w-full flex gap-6">
+        {/* left side - image upload section */}
+        <div className="md:w-[35%]">
+          {images?.length > 0 && (
+            <ImagePlaceholder
+              setOpenImageModal={setOpenImageModal}
+              size="765 x 850"
+              small={false}
+              index={0}
+              onImageChange={handleImageChange}
+              onRemove={handleRemoveImage}
+            />
+          )}
+          <div className="grid grid-cols-2 gap-3 mt-4">
+            {images.slice(1).map((_, index) => (
+              <ImagePlaceholder
+                setOpenImageModal={setOpenImageModal}
+                size="765 x 850"
+                key={index}
+                small
+                index={index + 1}
+                onImageChange={handleImageChange}
+                onRemove={handleRemoveImage}
+              />
+            ))}
+          </div>
+          <div className="mt-2">
+              <ColorSelector control={control} errors={errors} />
+              </div>
+        </div>
+        {/* right side - form inputs */}
+        <div className="md:w-[65%]">
+          <div className="w-full flex gap-6">
+            {/* product title input */}
+            <div className="w-2/4">
+              <Input
+                label="Product title"
+                placeholder="Enter product title"
+                {...register("title", { required: "Title is required" })}
+              />
+              {errors.title && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.title.message as string}
+                </p>
+              )}
+
+              <div className="mt-">
+                <Input 
+                type="textarea"
+                rows={7}
+                cols={10}
+                label="Short Description *(Max 200 words)"
+                placeholder="Enter product description for quick view"
+                {...register('description', {
+                  required: 'Description is required',
+                  validate: (value) => {
+                    const wordCount = value.trim().split(/\s+/).length;
+                    return (
+                      wordCount <= 200 || `Description cannot exceed 200 words (Current: ${wordCount}`
+                    )
+                  }
+                })}
+                />
+                {errors.description && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.description.message as string}
+                </p>
+              )}
+              </div>
+
+              <div className="mt-2">
+                <Input 
+                label="Tags *"
+                placeholder="apple,flagship,firebase"
+                {...register('tags', {
+                  required: 'Separate related products tags with a coma,'
+                })}
+                />
+                {errors.tags && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.tags.message as string}
+                </p>
+              )}
+              </div>
+
+              <div className="mt-2">
+                <Input 
+                label="Warranty *"
+                placeholder="1 Year / No warranty"
+                {...register('warranty', {
+                  required: 'Warranty is required'
+                })}
+                />
+                {errors.warranty && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.warranty.message as string}
+                </p>
+              )}
+              </div>
+
+              <div className="mt-2">
+                <Input 
+                label="Slug *"
+                placeholder="1 Year / No warranty"
+                {...register('slug', {
+                  required: 'Slug is required',
+                  pattern: {
+                    value: /^[a-z0-9]+(?:-[a-z0-9]+)*$/,
+                    message: "Invalid slug format! Only lowercase letters,numbers are allowed."
+                  },
+                  minLength: {
+                    value: 3,
+                    message: 'Slug must be at least 3 characters long.'
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: 'Slug cannot be longer than 50 characters long.'
+                  }
+                })}
+                />
+                {errors.slug && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.slug.message as string}
+                </p>
+              )}
+              </div>
+
+              <div className="mt-2">
+                <Input 
+                label="Brand"
+                placeholder="1 Year / No warranty"
+                {...register('brand', {
+                  required: 'Brand is required'
+                })}
+                />
+                {errors.brand && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.brand.message as string}
+                </p>
+              )}
+              </div>
+
+              <div className="mt-2">
+              <CustomSpecifications control={control} errors={errors} />
+              </div>
+              <div className="mt-2">
+              <CustomProperties control={control} errors={errors} />
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </form>
   );
 };
 
-export default CreateProduct
+export default CreateProduct;
