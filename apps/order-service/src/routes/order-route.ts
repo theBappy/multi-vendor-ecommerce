df561d45d9071dfb55126810981c865481001de4
theBappy <abappy575@gmail.com>
2025-07-26 14:32:54 +0600

    stripe payment intent, payment session, and create order business logic with all necessary redirection or confirmation

--- apps/order-service/src/routes/order-route.ts
+++ apps/order-service/src/routes/order-route.ts
@@ -1,6 +1,12 @@
-import express from 'express'
+import express, { Router } from 'express'
+import isAuthenticated from '@packages/middleware/isAuthenticated'
+import { createPaymentIntent, createPaymentSession, verifyPaymentSession } from '../controllers/order-controller'
 
 const router:Router = express.Router()
+
+router.post("/create-payment-intent", isAuthenticated, createPaymentIntent)
+router.post("/create-payment-session", isAuthenticated, createPaymentSession)
+router.get("/verifying-payment-session", isAuthenticated, verifyPaymentSession)
 
 
 export default router
